4f387747696a689291357b4f7956818edae08977
Vilmos Nebehaj <v.nebehaj@gmail.com>
2014-01-10 16:28:45 +0100

    First stab at RH.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,29 +1,33 @@
 # == Class: docker
 #
-# Module to install an up-to-date version of Docker from the
-# official Apt repository. The use of this repository means, this module works
-# only on Debian based distributions.
+# Module to install an up-to-date version of Docker from a package repository.
+# The use of this repository means, this module works only on Debian and Red
+# Hat based distributions.
 #
 class docker::install {
   validate_string($docker::version)
-  validate_re($::osfamily, '^Debian$', 'This module uses the docker apt repo and only works on Debian systems that support it.')
+  validate_re($::osfamily, '^(Debian|RedHat)$', 'This module only works on Debian and Red Hat based systems.')
   validate_string($::kernelrelease)
-  validate_bool($docker::use_upstream_apt_source)
+  validate_bool($docker::use_upstream_package_source)
 
-  if ($docker::use_upstream_apt_source) {
-    include apt
-    apt::source { 'docker':
-      location          => $docker::apt_source_location,
-      release           => 'docker',
-      repos             => 'main',
-      required_packages => 'debian-keyring debian-archive-keyring',
-      key               => 'A88D21E9',
-      key_source        => 'http://get.docker.io/gpg',
-      pin               => '10',
-      include_src       => false,
+  if ($docker::use_upstream_package_source) {
+    case $::osfamily {
+      'Debian': {
+        include apt
+        apt::source { 'docker':
+          location          => $docker::apt_source_location,
+          release           => 'docker',
+          repos             => 'main',
+          required_packages => 'debian-keyring debian-archive-keyring',
+          key               => 'A88D21E9',
+          key_source        => 'http://get.docker.io/gpg',
+          pin               => '10',
+          include_src       => false,
+        }
+
+        Apt::Source['docker'] -> Package['lxc-docker']
+      }
     }
-
-    Apt::Source['docker'] -> Package['lxc-docker']
   }
 
   case $::operatingsystemrelease {
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,12 +3,14 @@
 # Defaut parameter values for the docker module
 #
 class docker::params {
-  $version                 = undef
-  $ensure                  = present
-  $tcp_bind                = undef
-  $socket_bind             = 'unix:///var/run/docker.sock'
-  $use_upstream_apt_source = true
-  $service_state           = running
-  $root_dir                = undef
-  $apt_source_location     = 'https://get.docker.io/ubuntu'
+  $version                      = undef
+  $ensure                       = present
+  $tcp_bind                     = undef
+  $socket_bind                  = 'unix:///var/run/docker.sock'
+  $use_upstream_package_source  = true
+  $service_state                = running
+  $root_dir                     = undef
+  case $::osfamily {
+    'Debian': { $package_source_location = 'https://get.docker.io/ubuntu' }
+    'RedHat': { $package_source_location = '' }
 }
